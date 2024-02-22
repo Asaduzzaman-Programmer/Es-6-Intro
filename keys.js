aa9bdd893042c47102f7bd3e8d84bd2a450899ab
asad <asaduzzamanprogrammer@gmail.com>
2024-02-22 19:55:27 +0600

    complete module 30

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 const glass = { name:"toyota", color:"transparent", price: 12, isCleaned: true}
 console.log(glass.color, glass.name, glass.price, glass.isCleaned)
 
-const keys =Object.keys(glass)
+const keys =Object.entries(glass)
 console.log(keys)
