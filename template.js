891ce406ec5fa2f41f43df4458b7082c4df31ad5
asad <asaduzzamanprogrammer@gmail.com>
2024-02-22 15:55:55 +0600

    rare

--- template.js
+++ template.js
@@ -1,4 +1,7 @@
 const a = 10;
 const b = 20;
 const result = 'The sum of' + ' ' + a + " " + "and" + " " + b + " " + "is" + " " + (a+b);
-console.log(result)
+// console.log(result);
+
+const result2 = `The sum of ${a} and ${b} is ${a+b}`
+console.log(result2);
