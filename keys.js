b23dbda16151b61a7396aaec0dbe78a240207e0a
asad <asaduzzamanprogrammer@gmail.com>
2024-02-22 19:43:25 +0600

    30-8

---
+++ keys.js
@@ -0,0 +1,5 @@
+const glass = { name:"toyota", color:"transparent", price: 12, isCleaned: true}
+console.log(glass.color, glass.name, glass.price, glass.isCleaned)
+
+const keys =Object.keys(glass)
+console.log(keys)
