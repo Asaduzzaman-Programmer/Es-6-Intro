2bbe1f292f693221fecedc9176bac0db87832688
asad <asaduzzamanprogrammer@gmail.com>
2024-02-22 16:10:12 +0600

    arrow

---
+++ arrow.js
@@ -0,0 +1,26 @@
+const add = (a, b) => a+b;
+const addo = add (5, 7)
+console.log(addo)
+
+const sawon = (c, d) => c+d;
+const afifa = sawon(4, 4)
+console.log(afifa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
