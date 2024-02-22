bc9b4134894c0203100007b0c27e1dcdbd93b5a0
asad <asaduzzamanprogrammer@gmail.com>
2024-02-22 15:24:17 +0600

    fourth

--- var-let-const.js
+++ var-let-const.js
@@ -7,10 +7,23 @@ console.log(numbers)
 numbers.push(4, 8, 9);
 console.log(numbers)
 
-const student = {
+const studento = {
+    name: "moyna pakhi",
+    Result: "failed"
+}
+console.log(studento.Result)
+console.log(studento.name)
+
+let student = {
     name: "moyna pakhi",
     Result: "failed"
 }
 console.log(student.Result)
 console.log(student.name)
 
+student = {
+    name: "roman",
+    Result: "pass"
+}
+console.log(student.name)
+console.log(student.Result)
