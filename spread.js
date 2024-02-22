76856f25800ef75a468210c12f8cdfa8c4853f93
asad <asaduzzamanprogrammer@gmail.com>
2024-02-22 19:07:32 +0600

    spread

---
+++ spread.js
@@ -0,0 +1,9 @@
+// const max = Math.max(6,552,5894,65554);
+// console.log(max)
+
+
+
+const array = [544, 44, 55, 422, 4, 52, 4, 52, 4]
+const max = Math.max(...array);
+console.log(...array)
+console.log(max)
