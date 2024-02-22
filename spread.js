6c6286633b15ce132a0a48943ba99a7becf11870
asad <asaduzzamanprogrammer@gmail.com>
2024-02-22 19:24:51 +0600

    last

--- spread.js
+++ spread.js
@@ -3,7 +3,15 @@
 
 
 
-const array = [544, 44, 55, 422, 4, 52, 4, 52, 4]
-const max = Math.max(...array);
-console.log(...array)
-console.log(max)
+// const array = [544, 44, 55, 422, 4, 52, 4, 52, 4]
+// const max = Math.max(...array);
+// console.log(...array)
+// console.log(max)
+
+const friend = [4, 5, 6, 7, 8, 9];
+const bondhu = friend
+bondhu.push(45)
+console.log(bondhu);
+
+const dosto = [friend]
+console.log(...dosto, 45)
